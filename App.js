465b1c9b2ec6fe6514151055643fe02a5ec03b31
diva8287450 <diva_karan@ymail.com>
2020-10-20 16:29:10 +0800

    20oct

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import ComponentsScreen from "./src/screens/ComponentsScreen";
 import ListScreen from "./src/screens/ListScreen";
 import ImageScreen from "./src/screens/ImageScreen";
 import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
+import SquareScreen from "./src/screens/SquareScreen";
+
 
 const navigator = createStackNavigator(
   {
@@ -13,6 +16,8 @@ const navigator = createStackNavigator(
     List: ListScreen,
     Image: ImageScreen,
     Counter: CounterScreen,
+    Color: ColorScreen,
+    Square: SquareScreen,
   },
 
   {
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -27,6 +27,17 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Counter")}
         title="Go to Counter Demo"
       />
+
+      <Button
+        onPress={() => navigation.navigate("Color")}
+        title="Go to Color Demo"
+      />
+
+      <Button
+        onPress={() => navigation.navigate("Square")}
+        title="Go to Square Demo"
+      />
+      
     </View>
   );
 };
